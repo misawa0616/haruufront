1920a1e41018ab404c582ab8ec75b522f5e94659
misawa takuma <takumajane1@outlook.jp>
2020-08-22 15:53:41 +0900

    hoge

--- middleware/user_auth.js
+++ middleware/user_auth.js
@@ -1,8 +1,6 @@
 export default function ({ $axios, store, redirect, router }) {
     let url = "/api/v1/test/";
     return new Promise((resolve) => {
-        console.log('Token ')
-        console.log('Token ' + localStorage.getItem('token'));
         $axios.get(url)
             .then(function (res) {
                 store.dispatch("user/updateUserAction", res.data);
@@ -10,7 +8,6 @@ export default function ({ $axios, store, redirect, router }) {
             })
             .catch(function (e) {
                 store.dispatch("message/updateMessageAction", "有効なログイン情報を取得できませんでした。ログインしてください。");
-                console.log('Token ' + localStorage.getItem('token'));
                 return redirect('/g1_login')
             })
     });

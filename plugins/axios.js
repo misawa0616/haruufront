13d4b27f7f2d0a571971700256336cb148142a33
misawa takuma <takumajane1@outlook.jp>
2020-08-22 15:09:07 +0900

    hoge

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,7 @@
 export default ({ $axios }) => {
     // リクエストログ
     $axios.onRequest((config) => {
-        config.headers.common['Authorization'] = 'Token ' + sessionStorage.getItem('token');
+        config.headers.common['Authorization'] = 'Token ' + localStorage.getItem('token');
         config.headers.common['Content-Type'] = 'application/json';
     })
     // レスポンスログ
